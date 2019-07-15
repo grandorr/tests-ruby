77b27f67e123b7792354f0a876714b9a2aacd1cf
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-07-15 23:32:53 +0200

    Final

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -6,21 +6,52 @@ def translate(a)
 	voyel = ["a","e","i","o","u"]
 check = 0
 n = 0
+x = a 
+  words = a.split(/\W+/)
+  words.each do |a|
 	ok = voyel.include?(a[0])
-while check != 3
 	if ok == true 
 		a = a + "ay"
 		return a 
-	else
-    while ok == false
-	b = a.slice(0..n)
-	a = a + b + "ay"
+	end
+    while check <= 4
+	b = a.slice(0..check)
+
 	check = check + 1
-	return a[1..-1] 
+    
+	ok = voyel.include?(x[check])
+	test1 = "qu".include?(x[check])
+	if test1 == true 
+		if check == 1
+			b = a.slice(0..check)
+			a = a + b + "ay"
+		return a[2..-1]
+	    elsif check == 2
+	    	b = a.slice(1..check)
+	    		a = a + b + "ay"
+	    	return a[3..-1]
+	    elsif check == 3 
+	    		a = a + b + "ay"
+	    	return a[4..-1]
+	    end
+	end
+
+	if ok == true 
+		if check == 1
+			a = a + b + "ay"
+		return a[1..-1]
+	    elsif check == 2
+	    		a = a + b + "ay"
+	    	return a[2..-1]
+	    elsif check == 3 
+	    		a = a + b + "ay"
+	    	return a[3..-1]
+	    end
 	end
 end
 end
 end
+
 
 
 
